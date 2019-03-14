7356f255502f00e0287b915c0d7a388528f4c329
R. Alves Pinheiro <pinheiro.ras@gmail.com>
2019-03-13 22:29:56 -0300

    :sparkles: criando componentes diversao e restaurantes e suas rotas/navegação.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,14 @@ import { AppComponent } from "./app.component";
 import { TopoComponent } from "./topo/topo.component";
 import { HomeComponent } from "./home/home.component";
 import { RodapeComponent } from "./rodape/rodape.component";
+import { DiversaoComponent } from './diversao/diversao.component';
+import { RestaurantesComponent } from './restaurantes/restaurantes.component';
+import { RouterModule } from '@angular/router';
+import { ROUTES } from './app.routes';
 
 @NgModule({
-  declarations: [AppComponent, TopoComponent, HomeComponent, RodapeComponent],
-  imports: [BrowserModule, HttpClientModule],
+  declarations: [AppComponent, TopoComponent, HomeComponent, RodapeComponent, DiversaoComponent, RestaurantesComponent],
+  imports: [BrowserModule, HttpClientModule, RouterModule.forRoot(ROUTES)],
   providers: [],
   bootstrap: [AppComponent]
 })
