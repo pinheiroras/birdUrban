086d80f2b3c3a3c8c6afcb041332b3a97c994189
R. Alves Pinheiro <pinheiro.ras@gmail.com>
2019-03-14 13:12:51 -0300

    :sparkles: Gerando rotas com snapshot e subscribe\nExibindo a oferta dinamicamente\nMelhoria na URL do backend

--- README.md
+++ README.md
@@ -29,3 +29,18 @@ Run `ng e2e` to execute the end-to-end tests via [Protractor](http://www.protrac
 ## Further help
 
 To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI README](https://github.com/angular/angular-cli/blob/master/README.md).
+
+corrigir logo o pag down, imagem fica passando. :D
+
+Issues :
+
+- Melhorar o acesso da URL via configuração (environments)
+-
+
+Commit :
+
+Gerando rotas com snapshot e subscribe\n
+Exibindo a oferta dinamicamente\n
+Melhoria na URL do backend
+
+Aula : 130
--- src/app/ofertas.service.ts
+++ src/app/ofertas.service.ts
@@ -1,24 +1,37 @@
 import { Oferta } from "./shared/oferta.model";
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { URL_API } from "./oferta/oferta.api";
 
 //import "rxjs/add/operator/toPromise";
 
 @Injectable()
 export class OfertasService {
+  private url_api = `${URL_API}ofertas`;
+
   constructor(private http: HttpClient) {}
 
   public getOfertas(): Promise<Oferta[]> {
     return this.http
-      .get("http://localhost:3000/ofertas?destaque=true")
+      .get(`${this.url_api}?destaque=true`)
       .toPromise()
       .then((resposta: any) => resposta);
   }
 
   public getOfertasPorCategoria(categoria: string): Promise<Oferta[]> {
     return this.http
-      .get(`http://localhost:3000/ofertas?categoria=${categoria}`)
+      .get(`${this.url_api}?categoria=${categoria}`)
       .toPromise()
       .then((resposta: any) => resposta);
+  }
+
+  public getOfertaPorId(id: number): Promise<Oferta> {
+    return this.http
+      .get(`${this.url_api}?id=${id}`)
+      .toPromise()
+      .then((oferta: Oferta) => {
+        return oferta.shift();
+        //return oferta[0]    mesma coisa que o de cima
+      });
   }
 }
